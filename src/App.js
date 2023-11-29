5119c681d888304457a44a3674a51b624f8a5a10
LuisBetancourt2910 <144394515+LuisBetancourt2910@users.noreply.github.com>
2023-11-29 00:02:51 -0600

    First Commit
    This is the first commit to upload the web programming class project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Home from './components/Home/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CartContent from './components/CartContent/CartContent';
+import DataProvider from './components/Context/DataContext';
+
+function App() {
+  return (
+    <DataProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<CartContent />}/>
+      </Routes>
+    </BrowserRouter>
+    </DataProvider>
+  );
+}
+
+export default App;
+
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div className="banner">
+       <div className="banner-container">
+        <h1>Tienda de Perfumes</h1>
+        <p>Los mejores perfumes para ti</p>
+        </div>
+        </div>
+  )
+}
+export default Banner;
